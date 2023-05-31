b5fbb44a99a325ec85aa569586d304796e2bc5b1
Pouya Vedadiyan <vedadiyan@gmail.com>
2023-05-31 15:41:39 +0800

    code refactor

--- pkg/reflector.go
+++ pkg/reflector.go
@@ -21,34 +21,34 @@ func Reflect(validationTag string, message proto.Message, vc map[string]func() (
 		options := field.Options().(*descriptorpb.FieldOptions)
 		proto.RangeExtensions(options, func(et protoreflect.ExtensionType, i interface{}) bool {
 			fullName := et.TypeDescriptor().FullName()
-			if fullName == protoreflect.FullName(validationTag) {
-				rule, ok := i.(string)
-				if !ok {
-					return true
-				}
-				value := reflector.Get(field).Interface()
-				rules, err := ExprParser(rule)
-				if err != nil {
-					panic(err)
-				}
-				vc[field.TextName()] = func() (bool, error) {
-					output := true
-					for name, rule := range rules {
-						validator, ok := _rules[name]
-						if !ok {
-							return false, fmt.Errorf("%s: rule not found", name)
-						}
-						result, err := validator(value, rule)
-						if err != nil {
-							return false, err
-						}
-						output = output && result
-					}
-					return output, nil
-				}
-				return false
+			if fullName != protoreflect.FullName(validationTag) {
+				return true
 			}
-			return true
+			rule, ok := i.(string)
+			if !ok {
+				return true
+			}
+			value := reflector.Get(field).Interface()
+			rules, err := ExprParser(rule)
+			if err != nil {
+				panic(err)
+			}
+			vc[field.TextName()] = func() (bool, error) {
+				output := true
+				for name, rule := range rules {
+					validator, ok := _rules[name]
+					if !ok {
+						return false, fmt.Errorf("%s: rule not found", name)
+					}
+					result, err := validator(value, rule)
+					if err != nil {
+						return false, err
+					}
+					output = output && result
+				}
+				return output, nil
+			}
+			return false
 		})
 		if field.Kind() == protoreflect.MessageKind {
 			if field.IsList() {
