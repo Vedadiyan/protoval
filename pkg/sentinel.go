edef39a6c1ec0cc7d2b9529e05714221a03d21f0
Pouya Vedadiyan <vedadiyan@gmail.com>
2023-05-31 17:50:40 +0800

    code refactor

--- pkg/sentinel.go
+++ pkg/sentinel.go
@@ -5,8 +5,12 @@ type ValidationError struct {
 	message   string
 }
 
-func (protovalError ValidationError) Error() string {
-	return protovalError.message
+func (validationError ValidationError) FieldName() string {
+	return validationError.fieldName
+}
+
+func (validationError ValidationError) Error() string {
+	return validationError.message
 }
 
 func NewError(fieldName string, message string) *ValidationError {
